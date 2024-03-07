66b07d3f3507c57bbe733a6502b88730dc34f6ee
Dannte <daniel.lopez9804@gmail.com>
2024-03-07 12:11:06 -0500

    Added Game Event SO system

---
+++ Bubble Ernie/Assets/GameEvent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Event", menuName = "Scriptable Objects/Event")]
+public class GameEvent : ScriptableObject
+{
+    public List<GameEventListener> Listeners = new();
+
+    public void Register(GameEventListener listener)
+    {
+        if(Listeners.Contains(listener) == false)
+            Listeners.Add(listener);
+    }
+
+    public void Deregister(GameEventListener listener)
+    {
+        if (Listeners.Contains(listener) == true)
+            Listeners.Remove(listener);
+    }
+
+    public void Invoke()
+    {
+        foreach (GameEventListener listener in Listeners)
+            listener.Rise();
+    }
+}
